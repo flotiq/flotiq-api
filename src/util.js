2b99a3b7183da3cfbe9af78c6f2d5d4fb5634c42
Karol Grochowalski <karol.grochowalski@codewave.pl>
2026-05-13 15:59:50 +0200

    added utils

---
+++ src/util.js
@@ -0,0 +1,79 @@
+const fs = require("fs/promises");
+const util = require("util");
+const traverse = require("traverse");
+const glob = util.promisify(require('glob'));
+
+function camelize(str) {
+  return str
+    .replace(/(^|[_]+)([a-z])/g, (match, underscore, letter) => letter.toUpperCase());
+}
+
+async function readCTDs(directory) {
+  const CTDFiles = await glob(`${directory}/**/ContentTypeDefinition.json`)
+
+  return await Promise.all(
+      CTDFiles.map(fn => fs.readFile(fn, 'utf-8').then(JSON.parse))
+  )
+}
+
+async function shouldUpdate (relatedContentObject, replacements) {
+  return traverse(relatedContentObject).reduce(function (acc, node) {
+    if(this.key === 'dataUrl') {
+      const [,,,, ctd, id ] = node.split('/')
+      if (ctd === '_media') {
+        let haveReplacement = false;
+        for (const [ originalFile, replacementFile ] of replacements) {
+          if (id === originalFile.id) {
+            this.update(`/api/v1/content/${ctd}/${replacementFile.id}`)
+            haveReplacement = true;
+          }
+        }
+        return acc || haveReplacement;
+      }
+    }
+    return acc;
+  }, false)
+}
+
+function rateLimitInterceptor(axios, logger, defaultDelay = 1000) {
+  axios.interceptors.response.use(
+    response => response,
+    async function interceptRateLimit(error) {
+      if (error.response?.status === 429) {
+        const retryAfter = error.response.headers['retry-after'];
+        const waitTime = retryAfter ? Number.parseInt(retryAfter, 10) * 1000 : defaultDelay;
+        error.config._retryCount = (error.config._retryCount || 0) + 1;
+        logger.warn(`${error.config._retryCount} Received status 429 (Too Many Requests). Retrying after ${waitTime / 1000} seconds...`);
+        await new Promise(resolve => setTimeout(resolve, waitTime));
+
+        return axios.request(error.config); // Retry request
+      }
+
+      throw error;
+    });
+}
+
+function throttleInterceptor(axios, delay) {
+  let lastRequestTime = 0;
+
+  axios.interceptors.request.use(async function (config) {
+    const now = Date.now();
+    const timeSinceLastRequest = now - lastRequestTime;
+
+    if (timeSinceLastRequest < delay) {
+      const waitTime = delay - timeSinceLastRequest;
+      await new Promise(resolve => setTimeout(resolve, waitTime));
+    }
+
+    lastRequestTime = Date.now();
+    return config;
+  });
+}
+
+module.exports = {
+  camelize,
+  readCTDs,
+  shouldUpdate,
+  rateLimitInterceptor,
+  throttleInterceptor
+}
