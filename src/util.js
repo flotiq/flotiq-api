29066e67b92a5678fbde24630a2ba08330f5633a
Karol Grochowalski <karol.grochowalski@codewave.pl>
2026-05-13 16:07:21 +0200

    added glob dependencies

--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
 const fs = require("fs/promises");
 const util = require("util");
 const traverse = require("traverse");
-const glob = util.promisify(require('glob'));
+const { glob } = require('glob');
 
 function camelize(str) {
   return str
