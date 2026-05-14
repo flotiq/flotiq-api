d76768f779de003a0d7661c2cc749b4371d63ab6
Karol Grochowalski <karol.grochowalski@codewave.pl>
2026-05-14 09:37:21 +0200

    clean

--- src/util.js
+++ src/util.js
@@ -1,40 +1,3 @@
-const fs = require("fs/promises");
-const util = require("util");
-const traverse = require("traverse");
-const { glob } = require('glob');
-
-function camelize(str) {
-  return str
-    .replace(/(^|[_]+)([a-z])/g, (match, underscore, letter) => letter.toUpperCase());
-}
-
-async function readCTDs(directory) {
-  const CTDFiles = await glob(`${directory}/**/ContentTypeDefinition.json`)
-
-  return await Promise.all(
-      CTDFiles.map(fn => fs.readFile(fn, 'utf-8').then(JSON.parse))
-  )
-}
-
-async function shouldUpdate (relatedContentObject, replacements) {
-  return traverse(relatedContentObject).reduce(function (acc, node) {
-    if(this.key === 'dataUrl') {
-      const [,,,, ctd, id ] = node.split('/')
-      if (ctd === '_media') {
-        let haveReplacement = false;
-        for (const [ originalFile, replacementFile ] of replacements) {
-          if (id === originalFile.id) {
-            this.update(`/api/v1/content/${ctd}/${replacementFile.id}`)
-            haveReplacement = true;
-          }
-        }
-        return acc || haveReplacement;
-      }
-    }
-    return acc;
-  }, false)
-}
-
 function rateLimitInterceptor(axios, logger, defaultDelay = 1000) {
   axios.interceptors.response.use(
     response => response,
@@ -71,9 +34,6 @@ function throttleInterceptor(axios, delay) {
 }
 
 module.exports = {
-  camelize,
-  readCTDs,
-  shouldUpdate,
   rateLimitInterceptor,
   throttleInterceptor
 }
